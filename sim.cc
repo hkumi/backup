e4582334f5ef2c0554ce67e3b216d30c0fc7bd6e
hkumi <60217793+hkumi@users.noreply.github.com>
2024-04-11 10:11:33 +0200

    Add files via upload

--- sim.cc
+++ sim.cc
@@ -1,61 +1,84 @@
-#include <iostream>
-
-#include "G4RunManager.hh"
-#include "G4MTRunManager.hh"
 #include "G4UImanager.hh"
 #include "G4VisManager.hh"
 #include "G4VisExecutive.hh"
 #include "G4UIExecutive.hh"
 #include "QGSP_BERT_HP.hh"
-#include "G4RunManager.hh"
-#include "G4UIExecutive.hh"
-#include "G4VisManager.hh"
-#include "G4VisExecutive.hh"
-#include "Randomize.hh"
-#include "G4GeneralParticleSource.hh"
-
+#include "G4RunManagerFactory.hh"
 #include "construction.hh"
-#include "physics.hh"
-#include "FTFP_BERT.hh"
 #include "action.hh"
 #include "G4EmStandardPhysics_option4.hh"
 #include "G4OpticalPhysics.hh"
-
-int main(int argc, char** argv)
+#include "G4SteppingVerbose.hh"
+int main(int argc,char** argv)
 {
-    G4RunManager *runManager = new G4RunManager();
-    runManager->SetUserInitialization(new DetectorConstruction());
-     // Physics list
-    runManager->SetUserInitialization(new PhysicsList());   
-//      runManager->SetUserInitialization(new QGSP_BERT_HP());
+  // Detect interactive mode (if no arguments) and define UI session
+  //
+  G4UIExecutive* ui = nullptr;
+  if ( argc == 1 ) { ui = new G4UIExecutive(argc, argv); }
 
-    runManager->SetUserInitialization(new MyActionInitialization());
-    //runManager->Initialize();
-    G4UIExecutive *ui = 0;
-    if (argc ==1)
-    {
-       ui = new G4UIExecutive(argc,argv);
-    }
-    G4VisManager *visManager = new G4VisExecutive();
-    G4UImanager *UImanager =  G4UImanager::GetUIpointer();
+  // Optionally: choose a different Random engine...
+  // G4Random::setTheEngine(new CLHEP::MTwistEngine);
 
-    visManager->Initialize();
-    if (ui)
-    {
-       //UImanager->ApplyCommand("/control/execute vis.mac");
-       UImanager->ApplyCommand("/control/execute run.mac");
-     
-      
-       ui->SessionStart();
-    }
-    else
-    {
-       G4String command = "/control/execute";
-       G4String fileName = argv[1];
-       UImanager->ApplyCommand(command + fileName);
-    }
-    //job termination
-    delete visManager;
-    delete runManager;
+  //use G4SteppingVerboseWithUnits
+  G4int precision = 4;
+  G4SteppingVerbose::UseBestUnit(precision);
 
+  // Construct the default run manager
+  //
+  auto runManager = G4RunManagerFactory::CreateRunManager();
+
+  // Set mandatory initialization classes
+  //
+  // Detector construction
+  runManager->SetUserInitialization(new DetectorConstruction());
+
+  // Physics list
+  G4VModularPhysicsList* physicsList = new QGSP_BERT_HP;
+  physicsList->ReplacePhysics(new G4EmStandardPhysics_option4());
+  auto opticalPhysics = new G4OpticalPhysics();
+  physicsList->RegisterPhysics(opticalPhysics);
+  physicsList->SetVerboseLevel(1);
+  runManager->SetUserInitialization(physicsList);
+
+  // User action initialization
+  runManager->SetUserInitialization(new MyActionInitialization());
+
+  // Initialize visualization
+  //
+  G4VisManager* visManager = new G4VisExecutive;
+  // G4VisExecutive can take a verbosity argument - see /vis/verbose guidance.
+  // G4VisManager* visManager = new G4VisExecutive("Quiet");
+  visManager->Initialize();
+
+  // Get the pointer to the User Interface manager
+  G4UImanager* UImanager = G4UImanager::GetUIpointer();
+
+  // Process macro or start UI session
+  //
+  if ( ! ui ) {
+    // batch mode
+    G4String command = "/control/execute ";
+    G4String fileName = argv[1];
+    UImanager->ApplyCommand(command+fileName);
+  }
+  else {
+    // interactive mode
+    UImanager->ApplyCommand("/control/execute vis.mac");
+    ui->SessionStart();
+    delete ui;
+  }
+
+  // Job termination
+  // Free the store: user actions, physics_list and detector_description are
+  // owned and deleted by the run manager, so they should not be deleted
+  // in the main() program !
+
+  delete visManager;
+  delete runManager;
 }
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo.....
+
+
+
+
